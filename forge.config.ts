875e2d43f1bdaf6184f16c3dd0a9eedab7e28402
FernandoMaioli <fndmaioli5@gmail.com>
2024-02-02 13:24:58 -0300

    add app icon

--- forge.config.ts
+++ forge.config.ts
@@ -6,7 +6,9 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: 'src/public/Icons' // no file extension required
+  },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
